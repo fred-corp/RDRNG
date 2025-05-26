9d3de529741b9283eccc6dfe3a399c451c6c0410
Frédéric Druppel <Frederic.druppel@icloud.com>
2025-05-26 22:48:58 +0200

    [no ci] added read operations to testbench

--- sim/top/top_tb.vhd
+++ sim/top/top_tb.vhd
@@ -24,6 +24,8 @@ architecture tb of top_tb is
   signal led_b           : std_logic;
   signal debug           : std_logic;
 
+  signal spi_read_data : std_logic_vector(15 downto 0);
+
   ------------------------------------------------------------------------
   -- SPI Master Procedure
   ------------------------------------------------------------------------
@@ -45,6 +47,41 @@ architecture tb of top_tb is
       wait for sck_period / 2;
       sck <= '0';
     end loop;
+
+    cs <= '1';
+    wait for sck_period;
+  end procedure;
+
+  procedure spi_read(
+    signal sck          : out std_logic;
+    signal mosi         : out std_logic;
+    signal miso         : in std_logic;
+    signal cs           : out std_logic;
+    constant data       : std_logic_vector(15 downto 0);
+    constant read_bits : integer := 32;
+    signal read_data : out std_logic_vector(15 downto 0);
+    constant sck_period : time := 10 us
+  ) is
+  begin
+    cs <= '0';
+    wait for sck_period / 2;
+
+    for i in 15 downto 0 loop
+      mosi <= data(i);
+      wait for sck_period / 2;
+      sck <= '1';
+      wait for sck_period / 2;
+      sck <= '0';
+    end loop;
+    wait for sck_period / 2;
+    for i in read_bits-1 downto 0 loop
+      sck <= '1';
+      wait for sck_period / 2;
+      read_data(i) <= miso;
+      sck <= '0';
+    end loop;
+    wait for sck_period / 2;
+
 
     cs <= '1';
     wait for sck_period;
@@ -90,16 +127,34 @@ begin
     wait until rstn = '1';
     wait for 50 us;
 
-    -- Write operations
+    --* Write operations
+    -- Set LFSR mode
     spi_write(spi_sck, spi_mosi, spi_cs, x"00000001");
     wait for 100 us;
+    -- Set manual seed
     spi_write(spi_sck, spi_mosi, spi_cs, x"0001BEEF");
     wait for 100 us;
+    -- Turn on Red LED
     spi_write(spi_sck, spi_mosi, spi_cs, x"00100001");
     wait for 100 us;
 
-    -- Read operations
-    -- TODO
+    --* Read operations
+    -- Read a few numbers
+    for i in 1 to 10 loop
+      spi_read(spi_sck, spi_mosi, spi_miso, spi_cs, x"8000", 16, spi_read_data);
+      wait for 100 us;
+    end loop;
+    -- Reset manual seed
+    spi_write(spi_sck, spi_mosi, spi_cs, x"0001BEEF");
+    wait for 100 us;
+    -- Change polynomial
+    spi_write(spi_sck, spi_mosi, spi_cs, x"00030002");
+    wait for 100 us;
+    -- Read a few numbers
+    for i in 1 to 10 loop
+      spi_read(spi_sck, spi_mosi, spi_miso, spi_cs, x"8000", 16, spi_read_data);
+      wait for 100 us;
+    end loop;
 
     -- Example assertion (replace with meaningful test logic)
     check_equal(led_r, '0', "Expected LED R to be '0' after SPI command [xAA, x10, x00, X11]");
